0a4b9fadb677729f6b7b86de74fda2fedbe5bdf2
itzmespark116 <itzmespark116@gmail.com>
2026-08-06 16:46:19 +0530

    main-services section update 202608061664

--- js/main-team.js
+++ js/main-team.js
@@ -3,6 +3,30 @@
 // --------------------- main-team.js -------------------------
 // ------------------------------------------------------------
 
+const PLACEHOLDER_IMAGE = "img/placeholder-profile-pic.jpg";
+
+function imageFallback(src, alt) {
+    return `
+        <img 
+            class="main-team-image"
+            src="${src || PLACEHOLDER_IMAGE}"
+            alt="${alt}"
+            onerror="this.onerror=null;this.src='${PLACEHOLDER_IMAGE}'"
+        >
+    `;
+}
+
+function directorImageFallback(src, alt) {
+    return `
+        <img 
+            class="main-team-dir-image"
+            src="${src || PLACEHOLDER_IMAGE}"
+            alt="${alt}"
+            onerror="this.onerror=null;this.src='${PLACEHOLDER_IMAGE}'"
+        >
+    `;
+}
+
 fetch("js/team.json")
 .then(res => {
     if (!res.ok) {
@@ -20,28 +44,30 @@ fetch("js/team.json")
     }
 
     director.innerHTML = `
+        ${directorImageFallback(
+            team.director.image,
+            team.director.name
+        )}
 
-            <img class="main-team-image" src="${team.director.image}" alt="${team.director.name}">
+        <div class="main-team-dir-info">
+            <h3>${team.director.name}</h3>
+            <h4>${team.director.role}</h4>
 
-            <div class="main-team-info">
-                <h3>${team.director.name}</h3>
-                <h4>${team.director.role}</h4>
-
-                <ul>
-                    ${team.director.qualifications
+            <ul>
+                ${team.director.qualifications
                     .map(q => `<li>${q}</li>`)
                     .join("")}
-                </ul>
-            </div>
-
+            </ul>
+        </div>
     `;
 
-
     grid.innerHTML = team.employees.map(member => `
-
         <div class="main-team-card">
 
-            <img class="main-team-image" src="${member.image}" alt="${member.name}">
+            ${imageFallback(
+                member.image,
+                member.name
+            )}
 
             <div class="main-team-info">
                 <h3>${member.name}</h3>
@@ -49,13 +75,12 @@ fetch("js/team.json")
 
                 <ul>
                     ${member.qualifications
-                    .map(q => `<li>${q}</li>`)
-                    .join("")}
+                        .map(q => `<li>${q}</li>`)
+                        .join("")}
                 </ul>
-
             </div>
-        </div>
 
+        </div>
     `).join("");
 
 })
