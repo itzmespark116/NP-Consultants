0d05ffddcfbd8b829eb931630d9c5bd007b42640
itzmespark116 <itzmespark116@gmail.com>
2026-08-07 19:06:26 +0530

    placeholder html css init 202608071906

--- js/intro.js
+++ js/intro.js
@@ -4,7 +4,9 @@
 // ------------------------------------------------------------
 
 window.addEventListener("load",()=>{
+    document.body.classList.add("intro-active");
     setTimeout(()=>{
+        document.body.classList.remove("intro-active");
         document.querySelector(".intro").remove();
     },1500);
 });
